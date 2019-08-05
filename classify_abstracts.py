cbb1511dc97df4e1f31c3d403597931585aee3aa
Soumya Banerjee <soumyabanerjee@hotmail.com>
2019-08-05 11:43:37 +0530

    commit aftr 91.2 on 3 class- 20k

--- classify_abstracts.py
+++ classify_abstracts.py
@@ -14,8 +14,91 @@ from keras.models import Model, model_from_json, load_model
 from keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint
 import keras.backend as K
 import numpy as np
-import collections, os, re, traceback, pdb
+import collections, os, re, traceback, pickle, pdb
 #!import code; code.interact(local=vars())
+
+from spacy.lang.en import English
+
+def format_abstract(abstract):
+	nlp         	= English()
+	sentencizer 	= nlp.create_pipe("sentencizer")
+	nlp.add_pipe(sentencizer)
+
+
+	raw_data 			= []    
+	structured_data 	= [] 
+	structured_labels 	= []
+	buffered_abstract 	= []  
+	buffered_labels 	= [] 
+	
+
+	for sentence in nlp(abstract).sents:
+		line = sentence.strip()                                      
+		raw_data.append(line) # The original traning data  					
+
+		if line:                          
+			data = tt.tokenize(line)			
+			data =  data[1:] 
+			data = [word.lower() for word in data if word.isalnum()]
+
+			if len(data)<=3:
+				try:
+					buffered_abstract[-1]+=data # append to previous line					
+				except Exception as e:
+					buffered_abstract.append(data)
+			else:
+				buffered_abstract.append(data) 
+
+
+	if buffered_abstract:
+		structured_data.append(buffered_abstract)
+
+
+
+	X 		= []
+	X_words = []
+
+
+	for abstract in structured_data:
+		indexed_abstract 		= []
+		indexed_abstract_word 	= []
+		for sentence in abstract:
+			indexed_sentence 		= []
+			indexed_word_sentence 	= []
+			for word in sentence:
+				try:
+					index = word2idx[word]
+				except:
+					index = -1
+				indexed_sentence.append(index)
+				indexed_word = []
+				for char in word:
+					try:
+						char_index = char2idx[char]
+					except:
+						char_index = -1
+					indexed_word.append(char_index)
+				indexed_word_sentence.append(indexed_word)
+			# padding or pruning indexed_word_sentence as per nessacity
+			indexed_word_sentence = pad_sequences(indexed_word_sentence, maxlen=maxlen_word)
+			indexed_abstract.append(indexed_sentence)
+			indexed_abstract_word.append(indexed_word_sentence)
+
+		indexed_abstract  		= pad_sequences(indexed_abstract , maxlen=maxlen)
+		indexed_abstract_word  	= pad_sequences(indexed_abstract_word , maxlen=maxlen)
+		X.append(indexed_abstract)
+		X_words.append(indexed_abstract_word)
+
+
+	
+	X =  pad_sequences(X, abs_len,padding='post', value=0)
+	X_words =  pad_sequences(X_words, abs_len, padding='post', value=0)
+	# pdb.set_trace()
+	return X, X_words
+
+
+
+# abstract = "With the smart grid (SG) and the social Internet of Things (SIoT), an electric vehicle operator can use reliable, flexible, and efficient charging service with vehicle-to-grid (V2G). However, open channels can be vulnerable to various attacks by a malicious adversary. Therefore, secure mutual authentication for V2G has become essential, and numerous related protocols have been proposed. In 2018, Shen et al. proposed a privacy-preserving and lightweight key agreement protocol for V2G in SIoT to ensure security. However, we demonstrate that their protocol does not withstand impersonation, privileged-insider, and offline password guessing attacks, and it does not also guarantee secure mutual authentication, session key security, and perfect forward secrecy. Therefore, this paper proposes a dynamic privacy-preserving and lightweight key agreement protocol for V2G in SIoT to resolve the security weaknesses of Shen et al.'s protocol. The proposed protocol resists several attacks including impersonation, offline password guessing, man-in-the-middle, replay, and trace attacks, ensures anonymity, perfect forward secrecy, session key security, and secure mutual authentication. We evaluate the security of the proposed protocol using formal security analysis under the broadly-accepted real-or-random (ROR) model, secure mutual authentication proof using the widely-accepted Burrows-Abadi-Needham (BAN) logic, informal (non-mathematical) security analysis, and also the formal security verification using the broadly-accepted automated validation of Internet security protocols and applications (AVISPA) tool. We then compare computation costs, and security and functionality features of the proposed protocol with related protocols. Overall, the proposed protocol provides superior security, and it can be efficiently deployed to practical SIoT-based V2G environment."
 
 
 
@@ -128,7 +211,7 @@ def get_final_data(structured_data, structured_labels, data_dicts):
 	# pdb.set_trace()
 	return X, X_words, Y 
 
-def get_data(data_path, data_dicts = None, conv_to_3_class = True):
+def get_data(abstract, data_dicts):
 	# defauls : abs_len =30, maxlen=130, maxlen_word = 25
 	#  data_path = 'data/pubmed_non_rct.txt'
 	input_data = list(open(data_path, 'r'))
@@ -255,110 +338,23 @@ def get_labels(label2idx, Y_i):
 	idx2label =  dict((k,v) for v,k in label2idx.items())
 	return [idx2label[i] for i in Y_i] 
 
-def init_embedding_weights(i2w, w2vmodel):
-	# Create initial embedding weights matrix
-	# Return: np.array with dim [vocabsize, embeddingsize]
 
-	d = 300
-	V = len(i2w) -1  # -1 represents unknown words, thus removed from count
 
-	# pdb.set_trace()
-	assert sorted(i2w.keys())[1:] == list(range(V))  # verify indices are sequential
+def save_dictionaries(data_dicts, enbed_data, suffix):
+	print('Saving data dictionaries...')
+	serialized = pickle.dumps([data_dicts, enbed_data])
+	with open('saved_model/model_dict'+suffix+'.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+		f.write(serialized)
 
-	emb = np.zeros([V,d])
-	num_unknownwords = 0
-	unknow_words = []
-	for i,l in i2w.items():
-		if i==0:
-			continue
-		if i==-1 or l not in w2vmodel.vocab:
-			num_unknownwords += 1
-			unknow_words.append(l)
-			emb[i] = np.random.uniform(-1, 1, d)			
-		else:
-			emb[i, :] = w2vmodel[l]			
-	return emb, num_unknownwords, unknow_words 
-
-def model_basic(data_dicts, enbed_data):
-
-	(idx2word, _, _, _, abs_len , maxlen, maxlen_word, vocsize, charsize, nclasses) = data_dicts
-	(w2v , embed_dim, char_embed_dim) = enbed_data
+def load_dictionaries(suffix):
 	try:
-				
-		# Build the model
-		print('Building the model...')
-		
-		main_input = Input(shape=[abs_len, maxlen], dtype='int32', name='input') # (None, 36)
-		char_input = Input(shape=[abs_len, maxlen,  maxlen_word], dtype='int32', name='char_input') # (None, 36, 25)
-		# print 'passed checkpoint 1: input\n\n'
-		# pdb.set_trace()
-		main_input_r = Lambda(lambda x: K.reshape(x, shape=(-1, maxlen)))(main_input)
-
-		char_input_r = Lambda(lambda x: K.reshape(x, shape=(-1, maxlen, maxlen_word)))(char_input)
-
-		embeds, _, _  = init_embedding_weights( idx2word,  w2v)
-
-		# print 'passed checkpoint 2: embedding init\n\n'
-
-		embed = Embedding(input_dim= vocsize, output_dim= embed_dim, input_length= maxlen,
-						  weights=[embeds], mask_zero=False, name='embedding', trainable=True)(main_input_r)
-
-		embed = Lambda(lambda x: K.reshape(x, shape=[-1,abs_len, maxlen, embed_dim]))(embed)
-
-		# embed = Dropout(0.5, name='embed_dropout')(embed)
-
-
-		char_embed =  Embedding(input_dim=charsize, output_dim=char_embed_dim, embeddings_initializer='lecun_uniform', 
-						input_length=[maxlen, maxlen_word], mask_zero=False, name='char_embedding')(char_input_r)
-	 
-		char_embed_shape = char_embed.shape
-		char_embed = Lambda(lambda x: K.reshape(x, shape=(-1, maxlen_word, char_embed_dim)))(char_embed)
-			
-		# pdb.set_trace()	
-
-		biLSTM_char_embed = Bidirectional(LSTM(150, return_sequences=False))(char_embed)
-
-
-		# fwd_state = GRU(150, return_state=True)(char_embed)[-2]
-		# bwd_state = GRU(150, return_state=True, go_backwards=True)(char_embed)[-2]
-		# biLSTM_char_embed = Concatenate(axis=-1)([fwd_state, bwd_state])
-		char_embed = Lambda(lambda x: K.reshape(x, shape=[-1,abs_len, char_embed_shape[1], 2 * 150]))(biLSTM_char_embed)		
-
-		# char_embed = Dropout(0.5, name='char_embed_dropout')(char_embed)
-		# pdb.set_trace()
-
-		combined_embed = Concatenate( name='Sum')([embed, char_embed])
-
-		combined_embed = Lambda(lambda x: K.reshape(x, shape=(-1, maxlen, 2*embed_dim)))(combined_embed)
-		biLSTM = Bidirectional(LSTM(64, return_sequences=False))(combined_embed)
-		biLSTM = Dropout(0.5)(biLSTM)
-
-		biLSTM_r = Lambda(lambda x: K.reshape(x, shape=(-1, abs_len, 2*64)))(biLSTM)
-
-
-		norm = BatchNormalization()(biLSTM_r)
-		feedforward = Dense(nclasses, name='feed_forword')(norm)
-
-		final_output = CRF(nclasses, learn_mode='marginal', sparse_target=True)(feedforward)
-		# final_output = Activation('softmax')(feedforward) # (None, 36, 5) # (None, 36, 5)
-
-		
-		# print 'passed checkpoint 7: Final_classifier\n\n'
-
-		model = Model(inputs=[main_input, char_input], outputs=final_output, name='output')
-		model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-		# model.summary()
-		# plot_model(model, to_file='model.png', show_shapes=True)
-
-		return model
+		with open('saved_model/model_dict'+suffix+'.pkl', 'rb') as f:  # Python 3: open(..., 'wb')
+			raw = f.read()
+		[data_dicts, enbed_data] = pickle.loads(raw) 
+		return data_dicts, enbed_data
 	except Exception as e:
-		# print 'passed checkpoint E1\n\n'
-		# model.summary()
-		# plot_model(model, to_file='model.png', show_shapes=True)
-
-		traceback.print_exc()
-		pdb.set_trace() 
+		return None
+	
 
 def save_model_weight(model, sufix):
 	# serialize model to JSON
@@ -410,90 +406,19 @@ def get_results(pred, gold):
 
 	return acc, CM, non_pad_pred, non_pad_gold
 
-def main(num_epoch = 10, embed_top_n =  -1, load_model_weight_from_disk= True, save_model_weight_to_disk=True):
+def main(absract, sufix=''):
 	# Definition of some parameters
 	try:
-		
-		seed = 20
-		np.random.seed(seed)
-		
-		NUM_EPOCHS = num_epoch
-		BATCH_SIZE = 4
-
-		data_dicts, train, val, test, enbed_data =  data_fetch(embed_top_n)   # rest defaults	
-
-		(X_train, X_train_words, Y_train)	= train
-		(X_val, X_val_words, Y_val)			= val
-		(X_test, X_test_words, Y_test)		= test
-
-		save_file_sufix 					= '_'+str(X_train.shape[0] + X_val.shape[0] + X_test.shape[0])
-
-		# if load_model_from_disk:
-		# 	model =  load_model()
-		# else:
-		# 	model = None
-
-		# if model == None:
-		model = model_basic(data_dicts, enbed_data)
+		data_dicts, enbed_data  = load_dictionaries(sufix)		
+		save_file_sufix 		= sufix		
+		model 					= model_basic(data_dicts, enbed_data)
 
 		if load_model_weight_from_disk:
 			model_weights = load_model_weight(model, save_file_sufix)
 			if model_weights  is not None:
 				model = model_weights
 
-
-		plot_model(model, to_file='model'+save_file_sufix+'.png', show_shapes=True)
-
-
-		print('Training...')
-
-
-
-
-
-		
-		callbacks = [	TensorBoard(log_dir ='./logs', histogram_freq=0, write_graph=True, write_images=True),
-						EarlyStopping(monitor='val_loss', patience=2)#,
-      #        			ModelCheckpoint(filepath='best_model.ckpt', monitor='val_loss', save_best_only=True)
-             		]
-		# define model
-		# model.fit(X_train, Y_train,
-		#       batch_size=batch_size,
-		#       epochs=nb_epoch,
-		#       validation_data=(X_test, Y_test),
-		#       shuffle=True,
-		#       callbacks=[tensorboard])
-
-
-		# print 'passed checkpoint 11\n\n'
-		history = model.fit([X_train, X_train_words], Y_train, 
-						batch_size=BATCH_SIZE, 
-						# validation_split=0.1,
-		     			validation_data=([X_val, X_val_words], Y_val), 
-						epochs=NUM_EPOCHS, 
-						callbacks=callbacks)
-		# print 'passed checkpoint 9: training\n\n'
-
-		if save_model_weight_to_disk:
-			save_model_weight(model, save_file_sufix)
-
-
-		# forecast result
-		predir = '/users/debarshi/soumya/abstractAnalysis/ANMLAD/pubmed_non_rct/model_output/predictions'
-		fileprefix = 'embedding_level_attention_'
-
-		# scores = predict_score(model, [test_lex, pad_test_lex], test_toks, test_y, predir, idx2label, maxlen, fileprefix=fileprefix)
-
-
-		print('Predicting...')
-
-		pred_probs = model.predict([X_test, X_test_words], verbose=1)
-		# test_loss = model.evaluate(x=[test_lex, pad_test_lex], y=test_y, batch_size=1, verbose=1)
-		# pred = np.argmax(pred_probs, axis=1)
-		# golden = np.argmax(Y_test, axis=1)
-
-		# CM 	= confusion_matrix(golden, pred)
-		# acc = accuracy_score(golden, pred)
+		pred_probs = model.predict([X_test, X_test_words], verbose=0)
 		acc, CM, pred, gold = get_results(pred_probs, Y_test)
 
 
@@ -517,28 +442,28 @@ def main(num_epoch = 10, embed_top_n =  -1, load_model_weight_from_disk= True, s
 
 
 
-import sys
-args = sys.argv
+# import sys
+# args = sys.argv
 
-try:
-	num_epoch =  int(args[1])
-except Exception as e:
-	num_epoch 	= 10
-try:
-	embed_top_n =  int(args[2])
-except Exception as e:
-	embed_top_n 	= -1 # all
-try:
-	load_model_weight_from_disk =  Bool(args[3])
-except Exception as e:
-	load_model_weight_from_disk 	= True
-try:
-	save_model_weight_to_disk =  Bool(args[4])
-except Exception as e:
-	save_model_weight_to_disk 	= True
+# try:
+# 	num_epoch =  int(args[1])
+# except Exception as e:
+# 	num_epoch 	= 10
+# try:
+# 	embed_top_n =  int(args[2])
+# except Exception as e:
+# 	embed_top_n 	= -1 # all
+# try:
+# 	load_model_weight_from_disk =  Bool(args[3])
+# except Exception as e:
+# 	load_model_weight_from_disk 	= True
+# try:
+# 	save_model_weight_to_disk =  Bool(args[4])
+# except Exception as e:
+# 	save_model_weight_to_disk 	= True
 
 
 
-if __name__== "__main__":
-	# main(load_model_weight_from_disk= True, save_model_weight_to_disk=True, embed_top_n =  None):
-	main(num_epoch, embed_top_n, load_model_weight_from_disk, save_model_weight_to_disk)
+# if __name__== "__main__":
+# 	# main(load_model_weight_from_disk= True, save_model_weight_to_disk=True, embed_top_n =  None):
+# 	main(num_epoch, embed_top_n, load_model_weight_from_disk, save_model_weight_to_disk)
